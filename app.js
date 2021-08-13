29f627a9e519966bb3869e2c30cfac1e630bc9c2
gail enstice <skyedoll@aol.com>
2021-08-13 00:07:04 -0400

    imported packages and started team member questions

---
+++ app.js
@@ -0,0 +1,30 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require( "./lib/Intern");
+
+//const team = [];
+
+
+//function initApp() {
+  //  startHtml();
+    //addTeamMember();
+//}
+
+function addTeamMember() {
+    inquirer.prompt([{
+        message:"Enter team member's name",
+        name: "Name"
+    
+},
+{       
+    type: "list",
+    message: "Select team member's position",
+    choices: [
+        "Manager",
+        "Engineer",
+        "Manager",
+    ],
+
+}
