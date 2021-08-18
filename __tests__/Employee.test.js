bb3507d480d7cbe378860a0b76f04d106c7b1cdd
gail enstice <skyedoll@aol.com>
2021-08-17 23:15:07 -0400

    wrote tests for engineer and Intern

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,33 @@
+//using Employee constructor
+
+const Employee = require('../lib/Employee');
+
+
+//creates employee object
+test('creates an employee object', () => {
+    const employee = new Employee('');
+
+    expect(employee.name).toEqual(expect.any(String));
+    expect(employee.id).toEqual(expect.any(Number));
+    expect(employy.email).toEqual(expect.any(String));
+});
+
+//gets id from get id()
+test('gets employee name' , () => {
+    const employee = new Employee();
+    expect(employee.getName()).toEqual(expect.any(String));
+}) 
+
+//gets emails form getEmail()
+test('gets employee email' ,() => {
+    const employee = new Employee('Catherine');
+
+    expect(employee.getEmail()).toEqual(expect.stringContaining(employee.email.toString()));
+});
+
+//gets role from getRole()
+test('gets role of employee' ,() => {
+    const employee = new Employee ('Catherine');
+    
+    expect (employee.getRole()).toEqual("Employee");
+});
---
+++ __tests__/Engineer.test.js
@@ -0,0 +1,23 @@
+//using Engineer constructor
+
+const Engineer = require('../lib/Engineer');
+
+//creating engineer object
+test('creates an Engineer object',  () => {
+    const engineer = new Engineer(Catherine);
+    expect(engineer.github) .toEqual(expect.any(String));
+
+});
+//gets github from getGithub()
+test('gets engineer github value', () => {
+    const engineer = new Engineer(Catherine);
+    expect(engineer.getgithub()).toEqual(expect.stringContaining(engineer.github.toString()));
+});
+
+
+//gets role from getRole()
+test('gets role of employee' , () => {
+    const engineer = new Engineer (Catherine);
+    expect(engineer.getRole()).toEqual("Engineer");
+
+});
