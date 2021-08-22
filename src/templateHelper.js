10def2f6a7120b717ebbd01a192fa54b930b7566
gail enstice <skyedoll@aol.com>
2021-08-22 14:18:15 -0400

    created employee cards

--- src/templateHelper.js
+++ src/templateHelper.js
@@ -2,48 +2,127 @@
 const fs= require("fs");
 const path = require("path");
 const stylesheetcss = require("./stylesheetcss");
+//manager Card
+const generateManager = function(manager) {
+    return 
+    `<div class="col-4 mt-4">
+    <div class="card-header">
+    <h5 class="card-title">${manager.name}</h5>
+     <h5 class="card-title">${icon}${manager.getRole()}</h5>
 
-
-
+    </div>
+    <ul class="list-group list-group-flush">
+      <li class="list-group-item">ID: ${manager.id}</li>
+      <li class="list-group-item">Email: <a href=mailto:${manager.email}">${manager.email}<a></li>
+      <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
+      
+    </ul>
+  </div>;` 
+}   //engineer Card
+    const generateIntern = function (engineer) {
+        return
+        `<div class="col-4 mt-4">
+        <div class="card-header">
+        <h5 class="card-title">${engineer.name}</h5>
+         <h5 class="card-title">${icon}${engineer.getRole()}</h5>
     
+        </div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">ID: ${engineer.id}</li>
+          <li class="list-group-item">Email: <a href=mailto:${engineer.email}">${engineer.email}<a></li>
+         <li class="list-group-item"> Github: <a href="https://github.com/${engineer.github}/">${engineer.github}</a>
+          
+        </ul>
+      </div>;` 
+
+    }
+
+    //Intern Card
+      const generateIntern = function (intern) {
+          return
+          `<div class="col-4 mt-4">
+          <div class="card-header">
+          <h5 class="card-title">${intern.name}</h5>
+           <h5 class="card-title">${icon}${intern.getRole()}</h5>
+      
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">ID: ${intern.id}</li>
+            <li class="list-group-item">Email: <a href=mailto:${intern.email}">${intern.email}<a></li>
+            <li class="list-group-item"School: ${intern.school}></li>
+    </div>
+</div>
+      };`
+      
     //TODO:Generate Manager takes in manager and return html with manager details
     //TODO:Generate Engineer takes in manager and return html with maEngineernager details
     //TODO:Generate intern takes in manager and return html with intern details
 
 
-const createCards= (employees) => {
-    let HTML = "";
-    employees.forEach((employeeData) => {
-        let icon =
-        employee.getRole() =="Manager"
-        `<i class="fa-solid fa-user-tie"></i>` 
-        employees.getRole()=="Engineer"
-        `<i class="fa-solid fa-glasses-round"></i>`
-        employees.getRole()=="Intern"
-        `<i class="fa-solid fa-graduation-cap"></i>`
-        let other = employee.officeNumber
-            `office Number: ${employee.officeNumber}`
-            employee.github
-            `Github: <a href="https://github.com/${employee.github}/">${employee.github}</a>`
-             `School: ${employee.school}`;
+  //  let HTML = "";
+    //employees.forEach((employeeData) => {
+//const createCards= (employees) => {
+      //  let icon =
+        //employee.getRole() =="Manager"
+        //`<i class="fa-solid fa-user-tie"></i>` 
+        //employees.getRole()=="Engineer"
+        //`<i class="fa-solid fa-glasses-round"></i>`
+        //employees.getRole()=="Intern"
+        //`<i class="fa-solid fa-graduation-cap"></i>`
+        //let other = employee.officeNumber
+          //  `office Number: ${employee.officeNumber}`
+            //employee.github
+            //`Github: <a href="https://github.com/${employee.github}/">${employee.github}</a>`
+             //`School: ${employee.school}`;
 
-             HTML +=` <div class="card" style="width: 18rem;">
-             <div class="card-header">
-             <h5 class="card-title">${employee.name}</h5>
-              <h5 class="card-title">${icon}${employee.getRole()}</h5>
+             //HTML +=` <div class="card" style="width: 18rem;">
+             //<div class="card-header">
+             //<h5 class="card-title">${employee.name}</h5>
+              //<h5 class="card-title">${icon}${employee.getRole()}</h5>
 
-             </div>
-             <ul class="list-group list-group-flush">
-               <li class="list-group-item">ID: ${employee.id}</li>
-               <li class="list-group-item">Email: <a href=mailto:${employee.email}">${employee.email}<a></li>
-               <li class="list-group-item">${other}</li>
-             </ul>
-           </div>;`
-    });
-             generateTeamProfilePage(HTML);
+             //</div>
+             //<ul class="list-group list-group-flush">
+              // <li class="list-group-item">ID: ${employee.id}</li>
+               //<li class="list-group-item">Email: <a href=mailto:${employee.email}">${employee.email}<a></li>
+               //<li class="list-group-item">${other}</li>
+             //</ul>
+           //</div>;`
+    //});
 
-             let generateTeamProfilePage= (content) => {
-                 let HTML = `<!DOCTYPE html>
+             //push array to page
+             generateHTML = (data) => {
+
+                
+                for (let i=0; i <data.length; i++) {
+                    const employee = data[i];
+                    const role = employee.getRole();
+
+                    if (role==='Manager') {
+                        const ManagerCard = generateManager(employee);
+                        pageArray.push(ManagerCard);
+                    }
+
+                    if (role ==='Engineer') {
+                        const engineerCard = generateEngineer(employee);
+                        pageArray.push(EngineerCard)
+                    }
+                
+                if(role==='intern') {
+                    const internCard = generateIntern(employee);
+
+                    pageArray.push(internCard);
+                }
+
+                }
+                
+                const employeeCards = pageArray.join('')
+
+                const generateTeam = generateTeamPage(employeeCards);
+                
+
+             const generateTeamPage = function(employeeCards)  {
+                 
+                  `<!DOCTYPE html>
                  <html lang="en">
                  <head>
                  
@@ -64,7 +143,7 @@ const createCards= (employees) => {
                  </body>
                  </html>`
 
-
+             }
 
 
 
@@ -81,7 +160,7 @@ const createCards= (employees) => {
 
             };         
 //decalre html array
-        
+        generateTeamPage()
 //push
 
-module.exports = createCards;
+module.exports = generateHTML;
