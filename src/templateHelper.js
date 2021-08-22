a4df0d3a0f1cf9152e216ae8032f8e76f296f862
gail enstice <skyedoll@aol.com>
2021-08-22 00:20:07 -0400

    added html boiler plate and imported bootstrap

--- src/templateHelper.js
+++ src/templateHelper.js
@@ -16,21 +16,48 @@ const createCards= (employees) => {
     employees.forEach((employee) => {
         let icon =
         employee.generateTeamProfilePage() =="Manager"
-
+        `<i class="fa-solid fa-user-tie"></i>` 
         employees.generateTeamProfilePage()=="Engineer"
-
+        `<i class="fa-solid fa-glasses-round"></i>`
+        employees.generateTeamProfilePage()=="Intern"
+        `<i class="fa-solid fa-graduation-cap"></i>`
         let special = employee.officeNumber
             `office Number: ${employee.officeNumber}`
             employee.github
             `Github: <a href="https://github.com/${employee.github}/">${employee.github}</a>`
              `School: ${employee.school}`;
 
-             finalHTML +=
+             finalHTML +=` <div class="card" style="width: 18rem;">
+             <div class="card-header">
+             <h5 class="card-title">${employee.name}</h5>
+              <h5 class="card-title">${icon}${employee.getRole()}</h5>
 
+             </div>
+             <ul class="list-group list-group-flush">
+               <li class="list-group-item">ID: ${employee.id}</li>
+               <li class="list-group-item">Dapibus ac facilisis in</li>
+               <li class="list-group-item">Vestibulum at eros</li>
+             </ul>
+           </div>;`
+    });
              generateHTML(finalHTML);
 
              let generateHTML = (content) => {
-                 let html = 
+                 let html = `<!DOCTYPE html>
+                 <html lang="en">
+                 <head>
+                     <meta charset="UTF-8">
+                     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                     <link
+                    href= "https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js">
+                    <script src="https://kit.fontawesome.com/876c7fe23e.js" crossorigin="anonymous"></script>
+                     <title>My Team</title>
+                 </head>
+                 <body>
+                     
+                 </body>
+                 </html>`
 
 
 
@@ -43,13 +70,13 @@ const createCards= (employees) => {
              const writeToFile =(fileName,content) => {
                  //create file path
                  fs.writeFileSync(path.join(process.cwd(), "/dist/", fileName),content);
-
+             
              
              };
 
             };         
 //decalre html array
-
+        
 //push
 
 module.exports = createCards;
